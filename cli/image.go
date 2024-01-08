b07ced289eef30edc4e13fe28acbbda25cbd3d7f
Alex Athanasopoulos <alex@melato.org>
2024-01-08 15:53:41 +0200

    sort image instances by both image and instance

--- cli/image.go
+++ cli/image.go
@@ -6,6 +6,12 @@ import (
 
 type InstanceImageSorter []srv.InstanceImage
 
-func (t InstanceImageSorter) Len() int           { return len(t) }
-func (t InstanceImageSorter) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
-func (t InstanceImageSorter) Less(i, j int) bool { return t[i].Image < t[j].Image }
+func (t InstanceImageSorter) Len() int      { return len(t) }
+func (t InstanceImageSorter) Swap(i, j int) { t[i], t[j] = t[j], t[i] }
+func (t InstanceImageSorter) Less(i, j int) bool {
+	if t[i].Image != t[j].Image {
+		return t[i].Image < t[j].Image
+	} else {
+		return t[i].Instance < t[j].Instance
+	}
+}
