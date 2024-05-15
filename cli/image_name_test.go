0c930ca34e862912eed94b6bc5280ee55e32b2a8
Alex Athanasopoulos <alex@melato.org>
2024-05-15 22:25:35 +0300

    fixed PrefixNameParse test. #25

---
+++ cli/image_name_test.go
@@ -0,0 +1,22 @@
+package cli
+
+import (
+	"testing"
+)
+
+func TestPrefixNameParse(t *testing.T) {
+	var p PrefixNameTime
+	err := p.Parse("a-nginx-20240203-0834")
+	if err != nil {
+		t.Fatalf("%v", err)
+		return
+	}
+	expect := func(name, expected, value string) {
+		if value != expected {
+			t.Fatalf("%s=%s, expected: %s", name, value, expected)
+		}
+	}
+	expect("prefix", "a", p.Prefix)
+	expect("base", "nginx", p.Name)
+	expect("date", "20240203-0834", p.Time.UTC().Format("20060102-1504"))
+}
