fd91050a40d0dd4299cd573b64d96a71bdada253
Alex Athanasopoulos <alex@melato.org>
2024-05-17 13:05:43 +0300

    added options to "lxops publish" to set image properties. #25.

--- cli/publish.go
+++ cli/publish.go
@@ -2,33 +2,34 @@ package cli
 
 import (
 	"fmt"
-	"time"
-	"unicode"
+	"reflect"
 
+	"melato.org/lxops/srv"
 	"melato.org/lxops/yaml"
 )
 
 // PublishOps - publish instances
 type PublishOps struct {
 	InstanceOps
-	Name  string `name:"name" usage:"simple image name (one word, lowercase)`
-	Alias string `name:"alias" usage:"image alias`
+	Fields srv.ImageFields
+	Alias  string `name:"alias" usage:"image alias`
 }
 
-// capFirst - capitalize first rune of string
-func capFirst(s string) string {
-	if len(s) == 0 {
-		return ""
-	}
-	var s0 string
-	for i, c := range s {
-		if i == 0 {
-			s0 = string(unicode.ToUpper(c))
-		} else {
-			return s0 + s[i:]
+func (_ *PublishOps) mergeStructs(target, source any) {
+	t := reflect.TypeOf(source).Elem()
+	vIn := reflect.ValueOf(source).Elem()
+	vOut := reflect.ValueOf(target).Elem()
+	n := t.NumField()
+	fmt.Printf("%d fields\n", n)
+	for i := 0; i < n; i++ {
+		f := t.Field(i)
+		in := vIn.Field(i)
+		if !in.IsZero() {
+			fmt.Printf("set %s=%v\n", f.Name, in)
+			out := vOut.FieldByName(f.Name)
+			out.Set(in)
 		}
 	}
-	return s0
 }
 
 func (t *PublishOps) PublishInstance(instance, snapshot string) error {
@@ -36,28 +37,17 @@ func (t *PublishOps) PublishInstance(instance, snapshot string) error {
 	if err != nil {
 		return err
 	}
-	/*
-	  image.description: Alpinelinux 3.19 x86_64 (20240129_1300)
-	  image.name: alpinelinux-3.19-x86_64-default-20240129_1300
-	  image.os: alpinelinux
-	  image.release: "3.19"
-	  image.serial: "20240129_1300"
-	  image.variant: default
-	*/
-	serial := time.Now().UTC().Format("20060102_1504")
-	im.Serial = serial
-	name := t.Name
-	if name == "" {
-		name = instance
+	t.mergeStructs(im, &t.Fields)
+	if im.Name == "" {
+		im.Name = fmt.Sprintf("%s-%s-%s-%s-%s", im.OS, im.Release, im.Architecture, im.Variant, im.Serial)
 	}
-	im.Variant = name
+	if im.Description == "" {
+		im.Description = fmt.Sprintf("%s %s %s (%s)", im.OS, im.Release, im.Architecture, im.Serial)
+	}
 	alias := t.Alias
 	if alias == "" {
 		alias = instance
-		//alias = name + "-" + strings.ReplaceAll(serial, "_", "-")
 	}
-	im.Name = fmt.Sprintf("%s-%s-%s-%s-%s", im.OS, im.Release, im.Architecture, im.Variant, im.Serial)
-	im.Description = fmt.Sprintf("%s %s %s (%s)", capFirst(name), im.Release, im.Architecture, im.Serial)
 	return t.server.PublishInstanceWithFields(instance, snapshot, alias, *im)
 }
 
--- cli/publish_test.go
+++ cli/publish_test.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"fmt"
 	"testing"
+
+	"melato.org/lxops/srv"
 )
 
 func TestCapFirst(t *testing.T) {
@@ -17,4 +19,25 @@ func TestCapFirst(t *testing.T) {
 	verify("a", "A")
 	verify("one", "One")
 	verify("αβ", "Αβ")
+}
+
+func TestMergeStructs(t *testing.T) {
+	source := srv.ImageFields{
+		Name:    "name1",
+		Variant: "var1",
+	}
+	target := srv.ImageFields{
+		Release: "release2",
+		Variant: "var2",
+	}
+	var p PublishOps
+	p.mergeStructs(&target, &source)
+	expect := func(expected, actual string) {
+		if actual != expected {
+			t.Fatalf("expected: %s, actual: %s", expected, actual)
+		}
+	}
+	expect("name1", target.Name)
+	expect("var1", target.Variant)
+	expect("release2", target.Release)
 }
