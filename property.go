9bd0b5844051d561a610b5b3f5ad4b3a2e5afb4d
Alex Athanasopoulos <alex@melato.org>
2026-01-04 14:01:16 +0200

    use $HOME/config/lxops/properties.d/ to load multiple property files.  #30

--- property.go
+++ property.go
@@ -1,9 +1,12 @@
 package lxops
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"melato.org/lxops/util"
 	"melato.org/lxops/yaml"
@@ -12,6 +15,7 @@ import (
 type PropertyOptions struct {
 	PropertiesFile   string            `name:"properties" usage:"a file containing global config properties"`
 	GlobalProperties map[string]string `name:"-"`
+	userProperties   map[string]string
 }
 
 func (t *PropertyOptions) Init() error {
@@ -23,11 +27,56 @@ func (t *PropertyOptions) Init() error {
 	return nil
 }
 
+func ReadPropertiesDir(dir string, properties map[string]string) error {
+	_, err := os.Stat(dir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil
+		}
+	}
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	files := make([]string, 0, len(entries))
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			files = append(files, entry.Name())
+		}
+	}
+	sort.Strings(files)
+	for _, file := range files {
+		file = filepath.Join(dir, file)
+		err = yaml.ReadFile(file, properties)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (t *PropertyOptions) Configured() error {
+	t.GlobalProperties = make(map[string]string)
+	t.userProperties = make(map[string]string)
+	configDir, err := os.UserConfigDir()
+	if err == nil {
+		propertiesDir := filepath.Join(configDir, "lxops", "properties.d")
+		err = ReadPropertiesDir(propertiesDir, t.GlobalProperties)
+		if err != nil {
+			return err
+		}
+	}
+
 	if t.PropertiesFile != "" {
 		_, err := os.Stat(t.PropertiesFile)
 		if err == nil {
-			return yaml.ReadFile(t.PropertiesFile, &t.GlobalProperties)
+			err = yaml.ReadFile(t.PropertiesFile, t.userProperties)
+			if err != nil {
+				return err
+			}
+			for name, value := range t.userProperties {
+				t.GlobalProperties[name] = value
+			}
 		}
 	}
 	return nil
@@ -42,9 +91,10 @@ func (t *PropertyOptions) File() {
 }
 
 func (t *PropertyOptions) Set(key, value string) error {
-	if t.GlobalProperties == nil {
-		t.GlobalProperties = make(map[string]string)
+	if t.userProperties == nil {
+		t.userProperties = make(map[string]string)
 	}
+	t.userProperties[key] = value
 	t.GlobalProperties[key] = value
 	if t.PropertiesFile != "" {
 		dir := filepath.Dir(t.PropertiesFile)
@@ -52,7 +102,7 @@ func (t *PropertyOptions) Set(key, value string) error {
 		if err != nil {
 			return err
 		}
-		return yaml.WriteFile(t.GlobalProperties, t.PropertiesFile)
+		return yaml.WriteFile(t.userProperties, t.PropertiesFile)
 	}
 	return nil
 }
