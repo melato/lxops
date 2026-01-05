2b901ba3c194021f1e4a3d83e37263c9d9093ba3
Alex Athanasopoulos <alex@melato.org>
2026-01-05 21:17:39 +0200

    use a single condition property, instead of a list

--- cfg/condition.go
+++ cfg/condition.go
@@ -3,16 +3,10 @@ package cfg
 type HasProperty func(string) bool
 
 type Condition struct {
-	// Properties is a list of property names
-	// The condition passes if all properties with these names exist
-	Properties []string `yaml:"if,omitempty"`
+	// The condition passes if the property exists or is empty)
+	Property string `yaml:"if,omitempty"`
 }
 
 func (t *Condition) Eval(hasProperty HasProperty) bool {
-	for _, name := range t.Properties {
-		if !hasProperty(name) {
-			return false
-		}
-	}
-	return true
+	return t.Property == "" || hasProperty(t.Property)
 }
