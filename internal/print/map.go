915e3613a4d0e21cdee1d8642d36143a2ead6701
Alex Athanasopoulos <alex@melato.org>
2026-01-23 18:33:17 +0200

    added missing internal/print

---
+++ internal/print/map.go
@@ -0,0 +1,32 @@
+package print
+
+import (
+	"os"
+	"sort"
+
+	"melato.org/table3"
+)
+
+func MapKeys[T any](m map[string]T) []string {
+	keys := make([]string, 0, len(m))
+	for key, _ := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func PrintMap(m map[string]string) {
+	var key, value string
+	writer := &table.FixedWriter{Writer: os.Stdout}
+	writer.Columns(
+		table.NewColumn("KEY", func() interface{} { return key }),
+		table.NewColumn("VALUE", func() interface{} { return value }),
+	)
+	keys := MapKeys(m)
+	for _, key = range keys {
+		value = m[key]
+		writer.WriteRow()
+	}
+	writer.End()
+}
---
+++ internal/print/print_properties.go
@@ -0,0 +1,28 @@
+package print
+
+import (
+	"os"
+	"sort"
+
+	"melato.org/table3"
+)
+
+func PrintProperties(properties map[string]string) {
+	keys := make([]string, 0, len(properties))
+	for key, _ := range properties {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	var key, value string
+	writer := &table.FixedWriter{Writer: os.Stdout}
+	writer.Columns(
+		table.NewColumn("KEY", func() interface{} { return key }),
+		table.NewColumn("VALUE", func() interface{} { return value }),
+	)
+
+	for _, key = range keys {
+		value = properties[key]
+		writer.WriteRow()
+	}
+	writer.End()
+}
